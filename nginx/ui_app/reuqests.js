d575d4e6d4b61ee31d37b037703a84af6b2618f8
Wadowski Grzegorz <grzeg0132@gmail.com>
2024-10-25 11:33:15 +0200

    first but whole

--- nginx/ui_app/reuqests.js
+++ nginx/ui_app/reuqests.js
@@ -8,6 +8,15 @@ function sendGetRequest() {
     fetch(url)
         .then(response => response.json())
         .then(data => {
+            // pobierz referencję do elementu html o id getResponse,
+            // textContent potraktuj wszystko jako zwykłe znaki
+            // document.getElementById("getResponse").textContent = "Hello" ustawi w elemencie getResponse tekst "Hello"
+
+            // JSON.stringify() - zmienia obiekt JS (lub wartość) na format JSON
+            // data - obiekt, który chcę przekonwertować - odpowiedź serwera
+            // null - zamiast tego można wprowadzić funkcję modyfikującą sposób przekształcania obiektu na JSON
+            // 2 - sformatowanie wyniku, wcięcie nowej linii o 2 spacje
+
             document.getElementById("getResponse").textContent = JSON.stringify(data, null, 2);
         })
         .catch(error => console.error('Error:', error));
